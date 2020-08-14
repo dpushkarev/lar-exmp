752d39bd01a924bf88d1622d4eedd5110fd5d3b2
pushka <denis.pushkarev@corp.mail.ru>
2020-08-14 18:16:50 +0300

    fix error

--- app/Http/Resources/NemoWidget/Common/Airline.php
+++ app/Http/Resources/NemoWidget/Common/Airline.php
@@ -11,16 +11,16 @@ class Airline extends JsonResource
     public function toArray($request)
     {
         return [
-            $this->resource->code => [
-                "IATA" => $this->resource->code,
-                "name" => __($this->resource->name),
-                "nameEn" => $this->resource->name,
-                "nameFallback" => $this->resource->short_name,
-                "rating" => $this->resource->rating,
-                "countryCode" => $this->resource->country_code,
-                'logo' => $this->resource->logo,
-                'monochromeLogo' => $this->resource->monochromeLogo,
-                'colors' => $this->resource->colors
+            $this->resource->code ?? "" => [
+                "IATA" => $this->resource->code ?? null,
+                "name" => __($this->resource->name ?? null),
+                "nameEn" => $this->resource->name ?? null,
+                "nameFallback" => $this->resource->short_name ?? null,
+                "rating" => $this->resource->rating ?? null,
+                "countryCode" => $this->resource->country_code ?? null,
+                'logo' => $this->resource->logo ?? null,
+                'monochromeLogo' => $this->resource->monochromeLogo ?? null,
+                'colors' => $this->resource->colors ?? null
             ]
         ];
     }
